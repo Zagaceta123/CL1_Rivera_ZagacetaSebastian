c94919a0d4a88db842a96937076182468e051c3a
Zagaceta123 <rz_sebas@hotmail.com>
2024-06-07 16:11:38 -0500

    Examen

---
+++ src/main/java/pe/edu/cibertec/CL1_RiveraZagaceta_Sebastian/Cl1RiveraZagacetaSebastianApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_RiveraZagaceta_Sebastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1RiveraZagacetaSebastianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1RiveraZagacetaSebastianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL1_RiveraZagaceta_Sebastian/Cl1RiveraZagacetaSebastianApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_RiveraZagaceta_Sebastian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1RiveraZagacetaSebastianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
